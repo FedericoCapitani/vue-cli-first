8aec3f5ec00621f22d8ed22136d35ea94c4707f0
FedericoCapitani <federico.capitani@outlook.it>
2022-05-02 16:01:30 +0200

    create my-title component

---
+++ first-app/src/components/My-title.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="title">
+        <h1>La mia prima App Vue</h1>
+    </div>
+</template>
+
+<style>
+    .title {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: rgb(9, 49, 231);
+        border-radius: 10px;
+    }
+</style>
